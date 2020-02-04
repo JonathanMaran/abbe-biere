b19b05b1f3866ba297e70764abb1cd8a6528d612
maximesilva <maxime.silva@le-campus-numerique.fr>
2020-02-04 16:04:34 +0100

    modif layout et index

--- home.php
+++ home.php
@@ -1,23 +1,17 @@
 <?php
+include 'function.php';
 include 'pdo.php';
 include 'config.php';
 $description= 'le culte de la biere';
-?>
-        <div>
-            <?php foreach ($commentaires as $commentaire) {
-                ?> <p> <?php echo $commentaire['text']; ?><br><?php echo $commentaire['pseudo']; ?><br></p>
-                <ul class="nav nav-pills">
-                <li class="nav-item">
-                    <a class="nav-link "
-                       href="commentModify.php?idcomment=<?php echo $commentaire['id']; ?>&idarticle=<?php echo $idarticle; ?>">Modifier
-                        ce commentaire</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link "
-                       href="commentDelete.php?idcomment=<?php echo $commentaire['id']; ?>&idarticle=<?php echo $idarticle; ?>">Supprimer
-                        ce commentaire</a>
-                </li>
 
-                </ul><?php
-            } ?>
-        </div>';
+
+$products=productsindex($BDD);
+
+foreach ($products as $product) {
+    ?>
+    <a href="product.php?id=<?php echo $product['id']; ?>"><h2><?php echo $product['name']; ?></h2></a>
+    <p><?php echo $product['description']; ?></p>
+
+    <p><?php echo $product['price']; ?></p>
+    <?php
+} ?>
