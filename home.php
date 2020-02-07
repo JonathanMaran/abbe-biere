a896a4315348df437c56aac1647030d1a81c78dd
roy-amelie <roydorelon.amelie@gmail.com>
2020-02-07 09:29:37 +0100

    bouton ajout rapide page home fonctionnel

--- home.php
+++ home.php
@@ -1,12 +1,12 @@
 <?php
 
-//j inclus toutesles pages dont j'ai besoin
 
+if (isset($_POST['id'])){
+    $id = $_POST['id'];
+    addtoCart( $id, 1);
+}
 
-
-//variable description pour meta description
-
-
+debug($_SESSION);
 ?>
 
 <!-- main home-->
@@ -17,25 +17,28 @@
             <!-- titre -->
             <h2>Les derniers produits</h2>
         </div>
-<div class="row col-12 ">
-        <!-- affichage des derniers produits -->
-        <?php $products = productsindex($BDD);
-        foreach ($products as $product) {
-            echo '       
+        <div class="row col-12 ">
+            <!-- affichage des derniers produits -->
+            <?php $products = productsindex($BDD);
+            foreach ($products as $product) {
+                echo '       
                 <div class="card col-3 text-center shadow p-3 mb-5 " style="width: 16.5rem;">
                     <a href="index.php?page=products&id=' . $product['id'] . '"><img src="/photos/' . $product['photo_link'] . '.jpeg" class="card-img-top" alt="' . $product['photo_link'] . '"></a>
                     <div class="card-body">
                         <h5 class="card-title "><a href="index.php?page=products&id=' . $product['id'] . '">' . $product['name'] . '</a> </h5>
                         <p class="card-text">' . $product['description'] . '</p>
-                        <a href="index.php?page=products&id=' . $product['id'] . '"
-                           class="btn btn-secondary">' . $product['price'] . ' €</a>
+                            <form method="post">
+                                <input name="id" type="hidden" value="' . $product['id'] . '">
+                                <p>' . $product['price'] . ' €</p>
+                                <button type="submit" class="btn btn-dark">Ajout Rapide</button>
+                            </form>
                     
                     </div>
                 </div>';
 
 
-        } ?>
-</div>
+            } ?>
+        </div>
     </div>
 </main>
 
