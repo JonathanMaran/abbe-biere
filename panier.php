54741d5e5a2314581e344214e087fad03ef17c07
roy-amelie <roydorelon.amelie@gmail.com>
2020-02-08 18:25:44 +0100

    affichage panier uniquement si qte>0

--- panier.php
+++ panier.php
@@ -1,18 +1,20 @@
 <?php
-$message='';
-debug($_POST);
+$message = '';
+
+
+
 if (!empty($_POST)) {
     if (!empty($_POST['articles'])) {
 //        $qte = filter_input(INPUT_POST, 'qte', FILTER_VALIDATE_INT);
 //        $id = filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT);
         foreach ($_POST['articles'] as $id => $qte) {
 
-            $message=modifycart($BDD,$id, $qte);
+            $message = modifycart($BDD, $id, $qte);
         }
     }
-    if (!empty($_POST['delete'])){
-        foreach ($_POST['delete'] as $id=> $on){
-            $message=modifycart($BDD, $id,0);
+    if (!empty($_POST['delete'])) {
+        foreach ($_POST['delete'] as $id => $on) {
+            $message = modifycart($BDD, $id, 0);
         }
     }
 }
@@ -39,49 +41,51 @@ $totalprice = 0;
         } else {
         ?>
         <form class="form" method="post" action="index.php?page=panier">
-            <div class="col-12 text-center"><?=$message?></div><?php
+            <div class="col-12 text-center"><?= $message ?></div><?php
             foreach ($_SESSION['panier'] as $id => $qte) {
                 $product = view_product($BDD, $id);
                 $totalpriceproduct = $qte * $product['price'];
                 $totalprice += $qte * $product['price'];
+                if ($qte > 0) {
 
-                ?>
+                    ?>
 
 
-                <div class="col-12 text-center">
-                <div class="card mb-3" style="max-width: 1000px;">
-                    <div class="row no-gutters">
-                        <div class="col-md-4">
-                            <img src="/photos/<?= $product['photo_link'] ?>.jpeg" class="card-img"
-                                 alt="<?= $product['photo_link'] ?>">
-                        </div>
-                        <div class="col-md-4">
-                            <div class="card-body">
-                                <h5 class="card-title"><?= $product['name'] ?></h5>
+                    <div class="col-12 text-center">
+                    <div class="card mb-3" style="max-width: 1000px;">
+                        <div class="row no-gutters">
+                            <div class="col-md-4">
+                                <img src="/photos/<?= $product['photo_link'] ?>.jpeg" class="card-img"
+                                     alt="<?= $product['photo_link'] ?>">
                             </div>
-                        </div>
-                        <div class="col-md-4">
+                            <div class="col-md-4">
+                                <div class="card-body">
+                                    <h5 class="card-title"><?= $product['name'] ?></h5>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
 
-                            <label for="qte">Quantité</label>
-                            <input type="number" id="qte" min="0" name="articles[<?= $id ?>]" required minlength="4"
-                                   maxlength="8" size="10" value="<?= $qte ?>">
-                            <div class="form-check mb-2 mr-sm-2">
-                                <input class="form-check-input" type="checkbox" id="delete" name="delete[<?=$id?>]" value="on">
-                                <label class="form-check-label" for="delete">
-                                    Supprimer
-                                </label>
-                            </div>
-                            <div>
-                                <h7>prix unitaire : <?= $product['price'] ?> €</h7>
-                            </div>
-                            <div>
-                                <h7>prix total produit : <?= $totalpriceproduct ?> €</h7>
-                            </div>
+                                <label for="qte">Quantité</label>
+                                <input type="number" id="qte" min="0" name="articles[<?= $id ?>]" required minlength="4"
+                                       maxlength="8" size="10" value="<?= $qte ?>">
+                                <div class="form-check mb-2 mr-sm-2">
+                                    <input class="form-check-input" type="checkbox" id="delete"
+                                           name="delete[<?= $id ?>]" value="on">
+                                    <label class="form-check-label" for="delete">
+                                        Supprimer
+                                    </label>
+                                </div>
+                                <div>
+                                    <h7>prix unitaire : <?= $product['price'] ?> €</h7>
+                                </div>
+                                <div>
+                                    <h7>prix total produit : <?= $totalpriceproduct ?> €</h7>
+                                </div>
 
+                            </div>
                         </div>
                     </div>
-                </div>
-                </div><?php ;
+                    </div><?php }
             } ?>
             <div class="col-12 text-center">
                 <div>
